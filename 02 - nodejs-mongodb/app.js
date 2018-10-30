32e62d6f4c060f2afbb99479aa0300ad7008d795
pierangelo <pierangelo1982@gmail.com>
2018-10-30 21:02:26 +0100

    fixed mongo db

--- 02 - nodejs-mongodb/app.js
+++ 02 - nodejs-mongodb/app.js
@@ -8,7 +8,7 @@ const mongohost = 'mongodb://127.0.0.1:27017';
 // create a new MongoClient
 const client = new MongoClient(mongohost);
 
-// databse name
+// database name
 const dbName = 'demo';
 
 // connect to the server
@@ -19,6 +19,12 @@ client.connect(function(err) {
     // passo db
     const db = client.db(dbName);
     const collection = db.collection('agenda');
-
-    console.log("test", collection.find());
+    collection.find({}).toArray(function(err, docs){
+        assert.equal(err, null);
+        console.log("trovati i seguenti record");
+        console.log(docs);
+        for (i = 0; i < docs.length; i++) {
+            console.log(docs[i].nome + " " + docs[i].cognome + " " + docs[i].nazione);
+        }
+    });
 });

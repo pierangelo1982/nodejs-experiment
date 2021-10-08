9c9209815548a01c029588db402750da26d500a5
pierangelo1982 <pierangelo1982@gmail.com>
2021-10-08 19:03:33 +0200

    react in nodejs

---
+++ 013_node_react/js/app.js
@@ -0,0 +1,9 @@
+console.log('My Express React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+const title = 'My Simple Express React Webpack Babel Setup Environment';
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
